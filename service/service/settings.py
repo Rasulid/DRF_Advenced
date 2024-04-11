69f7da940615982a419724bce809f64f84e62500
Rasul Abduvaitov <rasulabduvaitov@gmail.com>
2024-04-11 15:33:10 +0500

    Day 2 - 2

--- service/service/settings.py
+++ service/service/settings.py
@@ -40,6 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'rest_framework',
+
     'clients.apps.ClientsConfig',
     'services.apps.ServicesConfig'
 ]

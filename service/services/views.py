ca89fd83f5641711a60b434e9b2faf09dfc6bf72
RasulAbduvaitov <rasulabduvaitov@gmail.com>
2024-04-15 15:21:14 +0500

    aggregation

--- service/services/views.py
+++ service/services/views.py
@@ -1,4 +1,4 @@
-from django.db.models import Prefetch
+from django.db.models import Prefetch, F, Sum
 from rest_framework.viewsets import ReadOnlyModelViewSet
 from .models import Subscription
 from .serializer import SupscriptionSerializer
@@ -8,7 +8,25 @@ from clients.models import Client
 class SubscriptionView(ReadOnlyModelViewSet):
     queryset = Subscription.objects.all().prefetch_related(
         'plan',
+        'service',
         Prefetch('client', queryset=Client.objects.all().select_related('user').only('company_name',
-                                                              'user__email')))
+                                                                                     'user__email'))
+    ).annotate(price=F('service__price') -
+                     F('service__price') *
+                     F('plan__discount_percent') / 100.00)
 
     serializer_class = SupscriptionSerializer
+
+    # aggregation function
+    def list(self, request, *args, **kwargs):
+        queryset = self.filter_queryset(self.get_queryset())
+
+        response = super().list(request, *args, **kwargs)
+
+        response_data = {
+            'result': response.data,
+            'total_amount': queryset.aggregate(total=Sum('price')).get('total')
+        }
+        response.data = response_data
+
+        return response
